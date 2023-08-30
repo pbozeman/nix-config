1f1223d9346bca0314b7c6e71b75d287269323b2
Patrick Bozeman <pbozeman@gmail.com>
2023-08-30 10:47:11 -0700

    add explicit emacs key bindings to zsh

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -105,6 +105,22 @@
       syntaxHighlighting.enable = true;
 
       initExtra = ''
+        # Setup preferred key bindings that emulate emacs.
+        bindkey '^P' up-history
+        bindkey '^N' down-history
+        bindkey '^?' backward-delete-char
+        bindkey '^h' backward-delete-char
+        bindkey '^w' backward-kill-word
+        bindkey '\e^h' backward-kill-word
+        bindkey '\e^?' backward-kill-word
+        bindkey '^r' history-incremental-search-backward
+        bindkey '^a' beginning-of-line
+        bindkey '^e' end-of-line
+        bindkey '\eb' backward-word
+        bindkey '\ef' forward-word
+        bindkey '^k' kill-line
+        bindkey '^u' backward-kill-line
+
         zstyle ':completion:*' completer _extensions _complete _approximate
         zstyle ':completion:*' menu select
         zstyle ':completion:*:manuals'    separate-sections true
