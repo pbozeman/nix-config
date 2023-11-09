d33ef74d18aa109921ef32afbfd9e8421ef9b0ce
Patrick Bozeman <pbozeman@gmail.com>
2023-11-09 19:28:20 +0000

    rename ubuntu-dev

--- flake.nix
+++ flake.nix
@@ -98,7 +98,7 @@
     };
 
     homeConfigurations = {
-      "parallels" = home-manager.lib.homeManagerConfiguration {
+      "ununtu-dev" = home-manager.lib.homeManagerConfiguration {
         pkgs = mkPkgs "x86_64-linux";
         extraSpecialArgs = {inherit inputs secrets user fullname email;};
         modules = [
