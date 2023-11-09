0f100b464115a92de65d73c1087a094cf9f122e2
Patrick Bozeman <pbozeman@gmail.com>
2023-11-09 13:05:28 -0800

    update lazyvim and move to ui/json xtras mgmt

--- home-manager/nvim/lua/config/lazy.lua
+++ home-manager/nvim/lua/config/lazy.lua
@@ -11,15 +11,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "plugins" },
   },
   defaults = {
