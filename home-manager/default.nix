8d1019914284e9f20728e3f0863719e95febf1d9
Patrick Bozeman <pbozeman@gmail.com>
2024-09-08 15:09:26 -0700

    [wez] atl-x key bindings for tabs
    while this was the default on osx, it wasn't on
    gnome/nixos.

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -575,6 +575,56 @@ in
               mods = 'CTRL',
               action = wezterm.action.PasteFrom('Clipboard'),
             },
+            {
+              key = 't',
+              mods = 'ALT',
+              action = wezterm.action.SpawnTab('DefaultDomain'),
+            },
+            {
+              key = '1',
+              mods = 'ALT',
+              action = wezterm.action.ActivateTab(0),
+            },
+            {
+              key = '2',
+              mods = 'ALT',
+              action = wezterm.action.ActivateTab(1),
+            },
+            {
+              key = '3',
+              mods = 'ALT',
+              action = wezterm.action.ActivateTab(2),
+            },
+            {
+              key = '4',
+              mods = 'ALT',
+              action = wezterm.action.ActivateTab(3),
+            },
+            {
+              key = '5',
+              mods = 'ALT',
+              action = wezterm.action.ActivateTab(4),
+            },
+            {
+              key = '6',
+              mods = 'ALT',
+              action = wezterm.action.ActivateTab(5),
+            },
+            {
+              key = '7',
+              mods = 'ALT',
+              action = wezterm.action.ActivateTab(6),
+            },
+            {
+              key = '8',
+              mods = 'ALT',
+              action = wezterm.action.ActivateTab(7),
+            },
+            {
+              key = '9',
+              mods = 'ALT',
+              action = wezterm.action.ActivateTab(8),
+            },
           },
           colors = {
             foreground = "#D8DEE9",
