a45b0e0fa6c58b7477e81c9461b8d3ef1165baf9
Patrick Bozeman <pbozeman@gmail.com>
2025-07-07 10:41:14 -0700

    [claude][kicad] use unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,14 +3,14 @@
 , ...
 }: {
   overlays = [
-    # (import ./claude-code.nix {
-    #   inherit inputs nixpkgs;
-    # })
+    (import ./claude-code.nix {
+      inherit inputs nixpkgs;
+    })
     # (import ./brave.nix {
     #   inherit inputs nixpkgs;
     # })
-    # (import ./kicad.nix {
-    #   inherit inputs nixpkgs;
-    # })
+    (import ./kicad.nix {
+      inherit inputs nixpkgs;
+    })
   ];
 }
