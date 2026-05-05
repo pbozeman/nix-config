984efaf4a06cbf7bc8b263c03de5cee1935c4830
Patrick Bozeman <pbozeman@gmail.com>
2026-05-05 09:02:02 -0700

    [git] use difftastic

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -17,12 +17,14 @@
       alias = {
         co = "checkout";
         br = "!git for-each-ref refs/heads --sort=committerdate --format='%(if)%(HEAD)%(then)*%(else) %(end)|%(refname:short)|%(committerdate:format:%Y-%m-%d %H:%M:%S)' | column -t -s '|'";
+        showd = "show --ext-diff";
       };
       color.ui = true;
       core = {
         editor = "nvim";
         autocrlf = "input";
       };
+      diff.external = "~/bin/ddifft";
       init.defaultBranch = "main";
       pull.rebase = true;
       rebase.autoStash = true;
